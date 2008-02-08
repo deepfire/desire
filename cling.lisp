8df9082ee2790ff7bada2b410b406b6b1718df89
Samium Gromoff <deepfire@elvees.com>
2008-02-08 07:34:48 +0300

    Namestringen path in svn->git puller.

--- cling.lisp
+++ cling.lisp
@@ -219,7 +219,7 @@
       (with-changed-directory path
         (git "svn" "fetch"))
       (with-changed-directory (repo-pool-root to)
-        (git "svn" "clone" (path from)))))
+        (git "svn" "clone" (namestring (path from))))))
 
 (defmethod pull ((to local-git-repository) (from local-darcs-repository))
   (ensure-directories-exist (namestring (path to)))
