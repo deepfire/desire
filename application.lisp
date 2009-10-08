3533901d779fdd8d621030be1a77502624fff375
Samium Gromoff <_deepfire@feelingofgreen.ru>
2009-10-09 01:21:02 +0400

    Somewhat fix application.

--- application.lisp
+++ application.lisp
@@ -23,9 +23,13 @@
 (defun define-application (name system package function &rest default-parameters)
   (make-instance 'application :name name :system (system system) :package package :function function :default-parameters default-parameters))
 
-(defun run (os &rest parameters &aux (o (coerce-to-application os)))
-  (cling (system-module (app-system o)) :skip-loadable t)
-  (apply (symbol-function (find-symbol (string (app-function o)) (app-package o))) (or parameters (app-default-parameters o))))
+(defun run (application-designator &rest parameters)
+  (let* ((a (coerce-to-application application-designator))
+         (s (app-system a)))
+    (unless (system-loadable-p s)
+      (lust (name s)))
+    (require (name s))
+    (apply (symbol-function (find-symbol (string (app-function a)) (app-package a))) (or parameters (app-default-parameters a)))))
 
-(defmethod purge-fasls ((o application))
-  (mapc #'purge-fasls (system-module (app-system o))))
+(defmethod purge-fasls ((a application))
+  (mapc #'purge-fasls (system-module (app-system a))))
