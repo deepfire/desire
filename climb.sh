10b696357a01571d4cb802402038dabf3a49f92d
Samium Gromoff <_deepfire@feelingofgreen.ru>
2009-10-11 02:59:36 +0400

    Re-drop the dots in climb.sh

--- climb.sh
+++ climb.sh
@@ -10,7 +10,7 @@ then
     EXPLAIN=nil
 else
     EXPLAIN=t
-    echo "NOTE: turning on execution explanation feature of desire."
+    echo "NOTE: turning on execution explanation feature of desire"
 fi
 
 if test -z "$DEBUG"
@@ -18,14 +18,14 @@ then
     DEBUG=0
 else
     DEBUG=3
-    echo "NOTE: optimising for debug."
+    echo "NOTE: optimising for debug"
 fi
 
 desire_home=git://$DESIRE_HOME
 
 if test -z "$RANDOM"
 then
-    echo "\$RANDOM is empty, nothing good will come out ot that -- please use GNU Bash."
+    echo "\$RANDOM is empty, nothing good will come out ot that -- please use GNU Bash"
     exit 1
 fi
 
@@ -45,7 +45,7 @@ fi
 
 if test ! "${ROOT:0:1}" = "/"
 then
-    echo "\$ROOT is not an absolute path."
+    echo "\$ROOT is not an absolute path"
     exit 1
 fi
 
@@ -53,15 +53,15 @@ desire_deps="alexandria cl-fad executor pergamum iterate semi-precious"
 temp_asdf_suffix="$USER-desire-temp-$RANDOM"
 temp_asdf_root="/tmp/$temp_asdf_suffix"
 
-test -z "$ROOT" && echo "ERROR: the first parameter must designate the desired location of the desire root." && exit 1
-test ! -d "$(dirname $ROOT)" && echo "ERROR: the parent directory of the first parameter must exist." && exit 1
-test -x "$ROOT" && echo "ERROR: the first parameter must specify a pathname not associated with any existing object." && exit 1
+test -z "$ROOT" && echo "ERROR: the first parameter must designate the desired location of the desire root" && exit 1
+test ! -d "$(dirname $ROOT)" && echo "ERROR: the parent directory of the first parameter must exist" && exit 1
+test -x "$ROOT" && echo "ERROR: the first parameter must specify a pathname not associated with any existing object" && exit 1
 
-echo "NOTE: will use \"$ROOT\" as root directory for mirror subroots."
-echo "NOTE: will use \"$temp_asdf_root\" as bootstrap package directory."
+echo "NOTE: will use \"$ROOT\" as root directory for mirror subroots"
+echo "NOTE: will use \"$temp_asdf_root\" as bootstrap package directory"
 
-mkdir $temp_asdf_root || ( echo "FATAL: unable to create the bootstrap package directory at \"$temp_asdf_root\", exiting." && exit 1 )
-mkdir $ROOT || ( echo "FATAL: unable to create the root directory at \"$ROOT\", exiting." && exit 1 )
+mkdir $temp_asdf_root || ( echo "FATAL: unable to create the bootstrap package directory at \"$temp_asdf_root\", exiting" && exit 1 )
+mkdir $ROOT || ( echo "FATAL: unable to create the root directory at \"$ROOT\", exiting" && exit 1 )
 mkdir $ROOT/git $ROOT/darcs $ROOT/hg $ROOT/cvs $ROOT/svn
 
 echo "NOTE: created required directories ok. Retrieving and loading desire and its dependencies:"
@@ -69,7 +69,7 @@ echo "NOTE: created required directories ok. Retrieving and loading desire and i
 for desire_dep in $desire_deps desire
 do
     echo -n "      $desire_dep: "
-    git clone $desire_home/$desire_dep "$temp_asdf_root/$desire_dep" >/dev/null || ( echo "FATAL: failed to retrieve $desire_dep." && exit 1 )
+    git clone $desire_home/$desire_dep "$temp_asdf_root/$desire_dep" >/dev/null || ( echo "FATAL: failed to retrieve $desire_dep" && exit 1 )
     echo "ok"
 done
 echo "NOTE: checking out branch $BRANCH of desire..."
