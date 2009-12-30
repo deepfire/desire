6658d7767c63f2acd8b9c0d3437c42a25856d9d2
Samium Gromoff <_deepfire@feelingofgreen.ru>
2009-12-30 06:08:02 +0300

    A couple methods for tarball remotes.

--- types.lisp
+++ types.lisp
@@ -349,13 +349,15 @@ a special module called '.meta'."
                      (hg 'hg-http-remote)
                      (darcs 'darcs-http-remote)
                      (svn 'svn-http-remote)
+                     (tarball 'tarball-http-remote)
                      ((nil) (definition-error "~@<The 'http' uri type is ambiguous, and there was no hint given.~:@>"))))
            ("cvs+rsync" 'cvs-rsync-remote)
            ("cvs" 'cvs-native-remote)
            (":pserver" 'cvs-native-remote)
            ("svn" 'svn-native-remote)
            ("svn+http" 'svn-http-remote)
-           ("svn+rsync" 'svn-rsync-remote))))
+           ("svn+rsync" 'svn-rsync-remote)
+           ("tarball+http" 'tarball-http-remote))))
 
 ;; Used for validation of user input in add-module
 (defun remote-types-compatible-p (x y)
