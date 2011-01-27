7f1964ae8b22f46df0c9fb342cfbee453a846c3c
Samium Gromoff <_deepfire@feelingofgreen.ru>
2011-01-28 00:55:57 +0300

    stale: move some code to Attic

--- types.lisp
+++ types.lisp
@@ -1410,51 +1410,6 @@ value is returned."
         ;; on the initially-obtain information.
         (merge-remote-wishmaster w)))))
 
-;;;
-;;; Desires.  Bitrotten and unused.
-;;;
-(defun distributor-related-desires (distributor-spec)
-  "Yield the names of modules currently desired from DISTRIBUTOR-SPEC."
-  (rest (find (coerce-to-name distributor-spec) *desires*)))
-
-(defun set-distributor-related-desires (new distributor-spec)
-  (setf (rest (find (coerce-to-name distributor-spec) *desires*)) new))
-
-(defsetf distributor-related-desires set-distributor-related-desires)
-
-(defun add-desire (distributor &optional (module-spec :everything) &aux
-                   (distributor (coerce-to-distributor distributor)))
-  "Add MODULE-SPEC (which is either a list of module specifications or an
-   :EVERYTHING wildcard) to the list of modules desired from DISTRIBUTOR."
-  (check-type module-spec (or (eql :everything) list))
-  (unionf (distributor-related-desires distributor)
-          (if (eq module-spec :everything)
-              (compute-distributor-modules distributor)
-              (mapcar #'coerce-to-name module-spec))))
-
-(defun module-desired-p (module &aux
-                         (module (coerce-to-module module)))
-  "See whether MODULE is desired. Return the desired distributor, if so."
-  (iter (for (distributor-name . desires) in *desires*)
-        (when (member (name module) desires)
-          (return (distributor distributor-name)))))
-
-(defun substitute-desires (in with)
-  "Substitute some of module->distributor maps in IN with those in WITH.
-
-   IN must be compounded (one specification per distributor), whereas WITH
-   is allowed to be spread, with many specifications per distributor."
-  (lret ((new-desires (copy-tree in)))
-    (iter (for (new-dist . modules) in with)
-          (iter (for module in modules)
-                (for olddistspec = (find module new-desires :key (compose (curry #'find module) #'rest)))
-                (unless (eq new-dist (car olddistspec))
-                  (when olddistspec
-                    (removef (rest olddistspec) module))
-                  (let ((new-home (or (find new-dist new-desires :key #'car)
-                                      (car (push (list new-dist) new-desires)))))
-                    (push module (rest new-home))))))))
-
 ;;;;
 ;;;; Localities.  Utilities not used anywhere at this moment.
 ;;;;
